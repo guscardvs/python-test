96266b5362e0b4e3fa5c544da55ccde55a317df9
Gustavo Correa <self.gustavocorrea@gmail.com>
2021-03-30 12:59:51 -0300

    added new examples

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def to_camel(string: str) -> str:
 def even_only(int_list: List[int]) -> List[str]:
     pass
 
-print(to_camel(text))
-print(even_only(integer_list))
+print(to_camel(text)) # Expected: snakeCase
+print(even_only(integer_list)) # Expected: list with only even numbers ex.: [2,4,6]
