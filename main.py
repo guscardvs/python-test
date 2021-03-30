05d436087471ef711db243c0d5bac1f306de3049
Gustavo Correa <self.gustavocorrea@gmail.com>
2021-03-30 19:42:53 -0300

    new challenge function

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import secrets
+from typing import Dict, List
+
 
 text = "snake_case"
 integer_list = [secrets.randbelow(10) for _ in range(10)]
+dictionary = {
+    "name":[
+        "Gustavo",
+        "Eduardo"
+    ],
+    "email":[
+        "self.gustavocorrea@gmail.com",
+        "edunascimento.mariano@gmail.com"
+    ]
+}
 
-def to_camel(string):
+def to_camel(string: str) -> str:
     ...
 
-def even_only(int_list):
+def even_only(int_list: List[int]) -> List[int]:
+    ...
+
+def to_record(dictionary: Dict[str, List[str]]) -> List[Dict[str, str]]:
     ...
 
 print(to_camel(text)) # Expected: snakeCase
 print(even_only(integer_list)) # Expected: list with only even numbers ex.: [2,4,6]
+print(to_record(dictionary)) 
+# Expected: 
+# [
+#   {"name":"Gustavo", "email":"self.gustavocorrea@gmail.com"}, 
+#   {"name":"Eduardo", "email":"edunascimento.mariano@gmail.com"}
+# ]
