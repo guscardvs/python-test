d26a91914ae3490a022007d7512ff737d621b52e
Gustavo Correa <self.gustavocorrea@gmail.com>
2021-03-30 13:34:27 -0300

    changed type annotation

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ text = "snake_case"
 integer_list = [secrets.randbelow(10) for _ in range(10)]
 
 def to_camel(string: str) -> str:
-    pass
+    ...
 
-def even_only(int_list: List[int]) -> List[str]:
-    pass
+def even_only(int_list: List[int]) -> List[int]:
+    ...
 
 print(to_camel(text)) # Expected: snakeCase
 print(even_only(integer_list)) # Expected: list with only even numbers ex.: [2,4,6]
